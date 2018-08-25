34a80a6e4751bbc592d1e95e23db191ea5303856
MohamedSayan <42085540+MohamedSayan@users.noreply.github.com>
2018-08-25 09:32:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,30 +1,89 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const devs = ['389090790984515594','350408440566382592'];
+var prefix = "!";
+const adminprefix = "!"
+const db = require('quick.db');
+const client = new Discord.Client();   
+const giphy = require('giphy-api')();    
+const googl = require('goo.gl');  
+const translate = require('google-translate-api');   
+const fs = require("fs"); 
+const canvas = require("canvas");
+const getYoutubeID = require('get-youtube-id'); 
+const moment = require("moment");  
+const { Client, Util } = require('discord.js');  
+const UserBlocked = new Set(); 
+const jimp = require('jimp');   
+const math = require('math-expression-evaluator'); 
+const stripIndents = require('common-tags').stripIndents;
+const figlet = require('figlet');
+const google = require('google-it'); 
+const queue = new Map(); 
+const zalgo = require('zalgolize');   
+const fetchVideoInfo = require('youtube-info');
+const YouTube = require('simple-youtube-api');
+const ytdl = require('ytdl-core');
+const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
+const sql = require("sqlite");
+ const dateFormat = require('dateformat'); 
+ const pretty = require('pretty-ms') 
 
-console.log("BOT ONLINE");
- 
-client.on("guildMemberAdd", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(` 
-**
-سلام عليكم :heart:
-لو سمحت ممكن تدخل سيرفرى  :heart:
-                               [ https://discord.gg/jExeAGW ] 
-الدعووة خاصة لك ... [ ${member}  ]
-**`) 
-}).catch(console.error)
+,ti={}  
+,spee={};
 
+
+client.on('message', message => {
+     if(!message.channel.guild) return;
+var prefix = "-";
+                if(message.content.startsWith(prefix + 'allbots')) {
+
+    
+    if (message.author.bot) return;
+    let i = 1;
+        const botssize = message.guild.members.filter(m=>m.user.bot).map(m=>`${i++} - <@${m.id}>`);
+          const embed = new Discord.RichEmbed()
+          .setAuthor(message.author.tag, message.author.avatarURL)
+          .setDescription(`**Found ${message.guild.members.filter(m=>m.user.bot).size} bots in this Server**
+${botssize.join('\n')}`)
+.setFooter(client.user.username, client.user.avatarURL)
+.setTimestamp();
+message.channel.send(embed)
+
+}
+
+
+});
+
+
+client.on('message', message => {
+            if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('!Mhstr')){
+ if (message.author.id !== '389090790984515594') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
+message.channel.sendMessage('جار ارسال الرسالة |✅')
+client.users.forEach(m =>{
+m.sendMessage(args)
 })
+}
+});
 
-client.on("guildMemberAdd", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(` 
-**
-سلام عليكم 
-لو سمحت ممكن تدخل سيرفرى
-                               [ https://discord.gg/jExeAGW ] 
-الدعووة خاصة لك ... [ ${member}  ]
-**`) 
-}).catch(console.error)
-})
-client.login(process.env.BOT_TOKEN);
+
+client.on('ready', function(){    
+    var ms = 40000 ;    
+    var setGame = ['!help','Syanah'];    
+    var i = -1;    
+    var j = 0;    
+    setInterval(function (){    
+        if( i == -1 ){    
+j = 1;    
+       }    
+        if( i == (setGame.length)-1 ){    
+            j = -1;    
+      }    
+       i = i+j;    
+        client.user.setGame(setGame[i],`https://www.youtube.com/channel/UCbYWJ6bYQ1pqZ6i-Lq8gCLA?view_as=subscriber`);    
+}, ms);    
+    
+});
+
+
