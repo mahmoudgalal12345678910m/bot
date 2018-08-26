463f50145473b7c703dab08df174a155eac9d684
MohamedSayan <42085540+MohamedSayan@users.noreply.github.com>
2018-08-26 10:46:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -707,8 +707,8 @@ const devs = ['396958215377780747'];
 client.on('message', message => {
 if(message.content === adminprefix + "restart") {
       if (!devs.includes(message.author.id)) return;
-          message.channel.send(⚠️ **الشخص الذي اعاد تشغيل البوت ${message.author.username}**);
-        console.log(⚠️ جاري اعادة تشغيل البوت... ⚠️);
+          message.channel.send( **الشخص الذي اعاد تشغيل البوت ${message.author.username}**);
+        console.log( جاري اعادة تشغيل البوت... );
         client.destroy();
         child_process.fork(__dirname + "/الملف.js");
         console.log(تم اعادة تشغيل البوت);
